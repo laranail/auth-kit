56300949ce3c3b013980953ea1c704d8d09479cc
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-07-20 11:20:43 +0300

    add request and controller classes
    Add request and controller classes for email/password login and email existence checks

---
+++ src/Http/Requests/AttemptEmailPasswordLoginRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Simtabi\Laranail\Auth\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AttemptEmailPasswordLoginRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'email'    => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+            'remember' => ['sometimes', 'boolean'],
+        ];
+    }
+}
