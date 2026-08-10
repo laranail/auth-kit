bf5aab43ffe06dd6674a67b0f0d53a33a5b214b1
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-08-10 07:27:55 +0300

    Injects LogoutUserInterface instead of concrete LogoutUser

--- src/Http/Controllers/AbstractLogoutController.php
+++ src/Http/Controllers/AbstractLogoutController.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace Simtabi\Laranail\Auth\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Simtabi\Laranail\Auth\Actions\LogoutUser;
+use Simtabi\Laranail\Auth\Contracts\LogoutUserInterface;
 
 abstract class AbstractLogoutController extends AbstractAuthController
 {
-    public function __invoke(Request $request, LogoutUser $action): mixed
+    public function __invoke(Request $request, LogoutUserInterface $action): mixed
     {
         $action->execute(guard: $this->guard());
 
