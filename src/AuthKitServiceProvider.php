973bc1cfcb1947ebf88142b2efb8f1b42f0eba17
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-08-12 10:03:12 +0300

    chore: delete the duplicate registration path

--- src/AuthKitServiceProvider.php
+++ src/AuthKitServiceProvider.php
@@ -32,8 +32,6 @@ class AuthKitServiceProvider extends ServiceProvider
         $this->app->bind(abstract: Contracts\CreatePendingEmailTokenInterface::class, concrete: Actions\CreatePendingEmailToken::class);
         $this->app->bind(abstract: Contracts\VerifyPendingEmailTokenInterface::class, concrete: Actions\VerifyPendingEmailToken::class);
         $this->app->bind(abstract: Contracts\SendPendingEmailTokenInterface::class, concrete: Actions\SendPendingEmailToken::class);
-        $this->app->bind(abstract: Contracts\RegisterInterface::class, concrete: Actions\Register::class);
-
         // Social actions
         $this->app->bind(abstract: Contracts\SocialRedirectActionInterface::class, concrete: Actions\SocialRedirectAction::class);
         $this->app->bind(abstract: Contracts\SocialCallbackActionInterface::class, concrete: Actions\SocialCallbackAction::class);
