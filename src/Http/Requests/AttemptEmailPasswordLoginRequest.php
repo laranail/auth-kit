faebe9b743875dbe7d612b8720cda35a1ab599fb
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-08-14 11:03:54 +0300

    chore: remove turnstiel from login

--- src/Http/Requests/AttemptEmailPasswordLoginRequest.php
+++ src/Http/Requests/AttemptEmailPasswordLoginRequest.php
@@ -4,19 +4,16 @@ declare(strict_types=1);
 
 namespace Simtabi\Laranail\Auth\Http\Requests;
 
-use Simtabi\Laranail\Auth\Support\AuthKit;
 use Illuminate\Foundation\Http\FormRequest;
 
 class AttemptEmailPasswordLoginRequest extends FormRequest
 {
     public function rules(): array
     {
-        return array_merge([
+        return [
             'email'    => ['required', 'string', 'email'],
             'password' => ['required', 'string'],
             'remember' => ['sometimes', 'boolean'],
-        ], [
-            config(key: 'auth-kit.turnstile.input', default: 'cf-turnstile-response') => AuthKit::turnstileRules(),
-        ]);
+        ];
     }
 }
